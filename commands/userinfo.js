27ae70122b8da2779047ec4c262471e1b6f5c914
tickedoffsquid <tickedoffsquid2@gmail.com>
2022-05-22 22:26:06 -0400

    Updated userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('serverinfo')
+		.setName('userinfo')
 		.setDescription('Replies with info about the given user!'),
 	async execute(interaction) {
 		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
